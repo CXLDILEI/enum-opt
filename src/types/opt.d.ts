f7f0abcb46742c60133dba7361a805335856c178
tangyunfei <451202165@qq.com>
2023-02-03 10:57:02 +0800

    feat: opt.d.ts

---
+++ src/types/opt.d.ts
@@ -0,0 +1,18 @@
+export interface Options {
+    label: string;
+    value: number;
+}
+export interface OptionsAstItem {
+    name: string;
+    options: Options;
+    startLine: number;
+    sourceEnum: string;
+    comments: string;
+}
+export interface ObjConfig {
+    [key: string]: string;
+}
+export interface Config {
+    entry: string | ObjConfig;
+    outDir: string;
+}
